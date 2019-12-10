d8a620ffe40238a26e0de52c627c3846e8d3a402
Tiago Santos <tiago.dos.santos@hitachivantara.com>
2019-12-10 16:28:56 +0000

    Update maven.yml

--- .github/actions/maven/entrypoint.sh
+++ .github/actions/maven/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
-set -eu
+set -e
 
-export MAVEN_CONFIG="${GITHUB_WORKSPACE:-$MAVEN_CONFIG}/.m2" # to be able to cache dependencies
+if [ -n "${GITHUB_WORKSPACE}" ]; then
+  export MAVEN_CONFIG="${GITHUB_WORKSPACE}/.m2" # to be able to cache dependencies
+fi
+
+# Configure user home
+export MAVEN_OPTS="$MAVEN_OPTS -Duser.home=${MAVEN_CONFIG%%/.m2}"
 
 exec /usr/local/bin/mvn-entrypoint.sh $@
