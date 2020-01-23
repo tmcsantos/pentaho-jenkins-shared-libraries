43f8420f0b6a0614034be86e5423b27e93a890e9
Tiago Santos <tiago.dos.santos@hitachivantara.com>
2020-01-23 14:58:59 +0000

    Update BasePipelineSpecification.groovy

--- test/src/org/hitachivantara/ci/BasePipelineSpecification.groovy
+++ test/src/org/hitachivantara/ci/BasePipelineSpecification.groovy
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+// Another change here
 package org.hitachivantara.ci
 
 import com.lesfurets.jenkins.unit.BasePipelineTest
